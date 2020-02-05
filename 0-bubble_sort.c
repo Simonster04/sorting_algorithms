4cb93331a8c1a5b8f875092739a9783f03dadab1
Simonster04 <simon.pahe@gmail.com>
2020-02-04 20:14:53 -0500

    correcting betty

--- 0-bubble_sort.c
+++ 0-bubble_sort.c
@@ -1,7 +1,7 @@
 #include "sort.h"
 
 /**
- * bubble_sort - sorts an array of ints in ascending order
+ * bubble_sort - sorts an array of ints ascending
  * @array: array with ints
  * @size: size of array
  * Return: void
@@ -15,17 +15,17 @@ void bubble_sort(int *array, size_t size)
 	{
 		return;
 	}
-	for(i = 0; i < size - 1; i++)
+	for (i = 0; i < size - 1; i++)
 	{
-		for(k = 0; k < size - 1; k++)
+		for (k = 0; k < size - 1; k++)
 		{
-			if(array[k] > array[k + 1])
+			if (array[k] > array[k + 1])
 			{
 				tmp = array[k];
 				array[k] = array[k + 1];
 				array[k + 1] = tmp;
 
-				for(j = 0; j < size; j++)
+				for (j = 0; j < size; j++)
 				{
 					printf("%d", array[j]);
 					if (j < size - 1)
