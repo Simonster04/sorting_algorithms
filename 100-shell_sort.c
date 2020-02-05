a5e7994ec9288b354c07c035dcedcec25891a20d
Simonster04 <simon.pahe@gmail.com>
2020-02-05 15:52:40 -0500

    first advanced

---
+++ 100-shell_sort.c
@@ -0,0 +1,28 @@
+#include "sort.h"
+
+/**
+ * shell_sort - sorts ascending w/ Knuth shellsort
+ * @array: array to be sorted
+ * @size: size of array
+ */
+
+void shell_sort(int *array, size_t size)
+{
+	int tmp;
+	size_t i, j, gap;
+
+	gap = 1;
+	for (gap = gap * 3 + 1; gap > 0; gap /= 3)
+	{
+		for (i = gap; i < size; i += 1)
+		{
+			tmp = array[i];
+			for (j = i; j >= gap && array[j - gap] > tmp; j -= gap)
+			{
+				array[j] = array[j - gap];
+			}
+			array[j] = tmp;
+		}
+		print_array(array, size);
+	}
+}
