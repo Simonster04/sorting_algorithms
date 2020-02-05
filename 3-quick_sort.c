6c72daa36b27d4bd3e0e6adf1269fd86c955efd3
Simonster04 <simon.pahe@gmail.com>
2020-02-05 09:12:07 -0500

    task #3

--- 3-quick_sort.c
+++ 3-quick_sort.c
@@ -4,54 +4,75 @@
  * quick_sort - sorts an array of integers w/ QUickSOrt
  * @array: array of integers to sort
  * @size: size of array
- *
  */
 
 void quick_sort(int *array, size_t size)
 {
-	size_t lo, hi;
+	ssize_t low, high;
 
-	lo = 0;
-	hi = size - 1;
-	quickSort(array, lo, hi);
-}
-
-
-void swap(int *a, int *b)
-{
-	int t = *a;
-
-	*a = *b;
-	*b = t;
-}
-
-
-int partition(int *array, size_t low, size_t high)
-{
-	int pivot = array[high];
-	size_t i = (low - 1), j;
-
-	for (j = low; j < high; j++)
- 	{
-		if (array[j] <= pivot)
-		{
-			i++;
-			swap(&array[i], &array[j]);
-		}
+	low = 0;
+	high = size - 1;
+	if (!array)
+	{
+		return;
 	}
-	swap(&array[i + 1], &array[high]);
-	return (i + 1);
+	quickSort(array, low, high, size);
 }
 
+/**
+ * quickSort - sorts a partition of array
+ * @array: array to be partitionated
+ * @low: lowest index in partition
+ * @high: highest index in the partition
+ * @size: size of the array
+ */
 
-void quickSort(int *array, size_t low, size_t high)
+void quickSort(int *array, ssize_t low, ssize_t high, size_t size)
 {
-	int pivot;
+	size_t pivot;
 
 	if (low < high)
 	{
-		pivot = partition(array, low, high);
-		quickSort(array, low, pivot - 1);
-		quickSort(array, pivot + 1, high);
+		pivot = partition(array, low, high, size);
+		quickSort(array, low, pivot - 1, size);
+		quickSort(array, pivot + 1, high, size);
 	}
+}
+
+/**
+ * partition - stroke the array in parts
+ * @array: array to be partitionated
+ * @low: lowest index in partition
+ * @high: highest index in the partition
+ * @size: size of the array
+ * Return: index of the partition
+ */
+
+size_t partition(int *array, ssize_t low, ssize_t high, size_t size)
+{
+	int pivot = array[high], tmp;
+	ssize_t i = (low - 1), j;
+
+	for (j = low; j < high; j++)
+	{
+		if (array[j] < pivot)
+		{
+			i++;
+			if (i != j)
+			{
+				tmp = array[i];
+				array[i] = array[j];
+				array[j] = tmp;
+				print_array(array, size);
+			}
+		}
+	}
+	if (array[i + 1] > array[high])
+	{
+		tmp = array[i + 1];
+		array[i + 1] = array[high];
+		array[high] = tmp;
+		print_array(array, size);
+	}
+	return (i + 1);
 }
